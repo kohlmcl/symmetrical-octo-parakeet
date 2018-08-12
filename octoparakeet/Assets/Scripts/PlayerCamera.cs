9368b1b083556c04dc871ae87b975670b8e1d5bd
kohlmcl <kohlmcl@gmail.com>
2018-08-12 12:47:42 -0400

    updated camera script

--- octoparakeet/Assets/Scripts/PlayerCamera.cs
+++ octoparakeet/Assets/Scripts/PlayerCamera.cs
@@ -4,21 +4,33 @@ public class PlayerCamera : MonoBehaviour {
 
     public Transform target;
     public float cameraSpeed = 4f;
+    public float cameraSmooth = 0.125f;
     public Vector3 cameraOffset;
+
+    //future control to stop camera when interacting with objects.
+    bool cameraMove = true;
 
     private void Start()
     {
         Cursor.visible = false;
     }
 
+
     private void LateUpdate()
     {
-        cameraOffset = Quaternion.Euler(Input.GetAxis("Mouse Y") * cameraSpeed,
-                                        Input.GetAxis("Mouse X") * cameraSpeed, 0) * cameraOffset;
+        if (cameraMove == true)
+        {
+            Vector3 desiredPosition = target.position + cameraOffset;
+            Vector3 smoothedPosition = Vector3.Lerp
+                                              (transform.position, desiredPosition, cameraSmooth);
 
-        transform.position = target.position + cameraOffset;
-        transform.LookAt(target.position);
+            cameraOffset = Quaternion.Euler(Input.GetAxis("Mouse Y") * cameraSpeed,
+                                            Input.GetAxis("Mouse X") * cameraSpeed, 0) * cameraOffset;
+
+            transform.position = smoothedPosition;
+            transform.LookAt(target.position);
         }
+    }
 
 }
 
