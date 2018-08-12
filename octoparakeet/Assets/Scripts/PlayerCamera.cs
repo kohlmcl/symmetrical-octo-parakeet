b8817738c84a46d6d563fbc75c28c52f3c8a73b0
kohlmcl <kohlmcl@gmail.com>
2018-08-11 20:31:28 -0400

    player camera + movement
    started working on the player camera & movement scripts.

---
+++ octoparakeet/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform target;
+    public float cameraSpeed = 4f;
+    public Vector3 cameraOffset;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void LateUpdate()
+    {
+        cameraOffset = Quaternion.Euler(Input.GetAxis("Mouse Y") * cameraSpeed,
+                                        Input.GetAxis("Mouse X") * cameraSpeed, 0) * cameraOffset;
+
+        transform.position = target.position + cameraOffset;
+        transform.LookAt(target.position);
+        }
+
+}
+
